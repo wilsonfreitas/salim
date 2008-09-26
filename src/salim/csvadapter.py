db40bc9d281b1c97ce61efb20547db3b1064c30d
Wilson Freitas <wilson.freias@gmail.com>
2008-09-26 01:05:11 +0000

    New function update_model_instance to update objects already store into database

--- src/salim/csvadapter.py
+++ src/salim/csvadapter.py
@@ -72,6 +72,29 @@ def resolve_property_names(properties, camel_case=False):
     return map(resolver, properties)
 
 
+def update_model_instance(store, obj):
+    db_obj = store.get(obj.__class__, getattr(obj, obj.__id__))
+    for key,value in obj._storm_kwargs.iteritems():
+        setattr(db_obj, key, value)
+    return db_obj
+
+
+def new_model_instance_with_parameter_less_init(typo, properties, values):
+    from storm.properties import PropertyColumn
+    from storm.references import Reference
+    obj = typo()
+    kwargs = {}
+    for prop,value in zip(properties, values):
+        if hasattr(typo, prop) and isinstance(getattr(typo, prop), (PropertyColumn, Reference)):
+            if hasattr(getattr(typo, prop), 'primary') and getattr(typo, prop).primary:
+                obj.__id__ = prop
+            else:
+                kwargs[prop] = value
+        setattr(obj, prop, value)
+    obj._storm_kwargs = kwargs
+    return obj
+
+
 def new_instance_with_parameter_less_init(typo, properties, values):
     obj = typo()
     for prop,value in zip(properties, values):
@@ -108,3 +131,24 @@ def parse_csv_file(csv_file, globals, camel_case=False, parameter_less=True, par
             values = map(lambda v: parse_value(v, parse_table), row[1:])
             objects.append(create_new_instance(typo, properties, values, parameter_less))
     return objects
+
+
+def parse_model_csv_file(csv_file, globals, camel_case=False, parameter_less=True, parse_table=parse_table):
+    '''
+    Read the csv file and instanciate objects.
+    
+    If only one entry point is enabled, the csv setting must be given here.
+    '''
+    objects = []
+    for row in csv.reader(csv_file):
+        row = [field.strip() for field in row]
+        if len(row) == 0 or row[0].startswith('#') or (len(row) == 1 and row[0] == ''): # ignore comments and empty lines
+            continue
+        if bool(row[0]):
+            typo, properties = globals[row[0]], resolve_property_names(row[1:], camel_case=camel_case)
+        else:
+            values = map(lambda v: parse_value(v, parse_table), row[1:])
+            objects.append(new_model_instance_with_parameter_less_init(typo, properties, values))
+    return objects
+
+
